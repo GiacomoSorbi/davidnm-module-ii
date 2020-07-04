8cfff523aaf2165aeded0f88a3b9d7d3c17aa326
David NoMa <davidnm@gmail.com>
2020-07-04 11:01:49 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://GiacomoSorbi.github.io/davidnm-module-ii/precache-manifest.9b46ffdd8f8b6e98829610f2a3bed589.js"
+  "/davidnm-module-ii/precache-manifest.1590c6af1c9eeeb64d029026c24819a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://GiacomoSorbi.github.io/davidnm-module-ii/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/davidnm-module-ii/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
