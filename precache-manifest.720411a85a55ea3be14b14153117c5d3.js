1fa8528b475869f32d57b1d9dc7a8eb9072a54d0
David NoMa <davidnm@gmail.com>
2020-07-15 13:40:18 +0100

    Updates

--- precache-manifest.720411a85a55ea3be14b14153117c5d3.js
+++ precache-manifest.720411a85a55ea3be14b14153117c5d3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "15cab8479b4976665ffbdc2404d1930f",
+    "revision": "e921c3bcf9b733631f595a2fcdcb9483",
     "url": "/davidnm-module-ii/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/davidnm-module-ii/static/css/2.1f156bdb.chunk.css"
   },
   {
-    "revision": "cea36159231db09fc94d",
-    "url": "/davidnm-module-ii/static/css/main.607e82a4.chunk.css"
+    "revision": "003c910a3c93a5ecfe91",
+    "url": "/davidnm-module-ii/static/css/main.2a40d103.chunk.css"
   },
   {
     "revision": "4ecfd5a85013472f7892",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/davidnm-module-ii/static/js/2.37b9782c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cea36159231db09fc94d",
-    "url": "/davidnm-module-ii/static/js/main.44825f98.chunk.js"
+    "revision": "003c910a3c93a5ecfe91",
+    "url": "/davidnm-module-ii/static/js/main.918d6196.chunk.js"
   },
   {
     "revision": "fe9043da7e86fdb3d354",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/davidnm-module-ii/precache-manifest.21718c2c57735291584b661447945f9b.js"
+  "/davidnm-module-ii/precache-manifest.720411a85a55ea3be14b14153117c5d3.js"
 );
 
 self.addEventListener('message', (event) => {
